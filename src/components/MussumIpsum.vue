d33f306bc4cbae4a7f0872d519d50b8e0f904839
Jeudi Prando <jeudiprando@gmail.com>
2023-06-29 14:34:07 -0300

    client visible é lindo

---
+++ src/components/MussumIpsum.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss, é um leite
+    divinis, qui tem lupuliz, matis, aguis e fermentis.Detraxit consequat et quo
+    num tendi nada.Atirei o pau no gatis, per gatis num morreus.Quem num gosta
+    di mé, boa gentis num é. Leite de capivaris, leite de mula manquis sem
+    cabeça.Interessantiss quisso pudia ce receita de bolis, mais bolis eu num
+    gostis.Copo furadis é disculpa de bebadis, arcu quam euismod magna.Vehicula
+    non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum. A
+    ordem dos tratores não altera o pão duris.Quem num gosta di mé, boa gentis
+    num é.In elementis mé pra quem é amistosis quis leo.Paisis, filhis,
+    espiritis santis. Quem manda na minha terra sou euzis!Paisis, filhis,
+    espiritis santis.Nullam volutpat risus nec leo commodo, ut interdum diam
+    laoreet. Sed non consequat odio.Não sou faixa preta cumpadi, sou preto
+    inteiris, inteiris. Mauris nec dolor in eros commodo tempor. Aenean aliquam
+    molestie leo, vitae iaculis nisl.Praesent vel viverra nisi. Mauris aliquet
+    nunc non turpis scelerisque, eget.Quem num gosta di mim que vai caçá sua
+    turmis!Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo
+    pose. Manduma pindureta quium dia nois paga.Nullam volutpat risus nec leo
+    commodo, ut interdum diam laoreet. Sed non consequat odio.Interagi no mé,
+    cursus quis, vehicula ac nisi.Aenean aliquam molestie leo, vitae iaculis
+    nisl. Viva Forevis aptent taciti sociosqu ad litora torquent.Pra lá , depois
+    divoltis porris, paradis.In elementis mé pra quem é amistosis quis
+    leo.Aenean aliquam molestie leo, vitae iaculis nisl. Delegadis gente finis,
+    bibendum egestas augue arcu ut est.Viva Forevis aptent taciti sociosqu ad
+    litora torquent.Admodum accumsan disputationi eu sit. Vide electram
+    sadipscing et per.Casamentiss faiz malandris se pirulitá. Quem num gosta di
+    mim que vai caçá sua turmis!Nullam volutpat risus nec leo commodo, ut
+    interdum diam laoreet. Sed non consequat odio.Não sou faixa preta cumpadi,
+    sou preto inteiris, inteiris.Posuere libero varius. Nullam a nisl ut ante
+    blandit hendrerit. Aenean sit amet nisi. Praesent malesuada urna nisi, quis
+    volutpat erat hendrerit non. Nam vulputate dapibus.Pra lá , depois divoltis
+    porris, paradis.Quem manda na minha terra sou euzis!Si num tem leite então
+    bota uma pinga aí cumpadi!
+  </section>
+</template>
--- src/components/Teste.vue
+++ src/components/Teste.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
 import PrimeButton from "primevue/button";
+import PrimeCard from "primevue/card";
 </script>
 
 <template>
-  <PrimeButton> teste </PrimeButton>
+  <PrimeCard style="margin: auto; width: 300px">
+    <template #header> header </template>
+    <template #title> title </template>
+    <template #subtitle> subtitle </template>
+    <template #content>
+      <div>
+        <PrimeButton> teste </PrimeButton>
+      </div>
+      <div>
+        <slot />
+      </div>
+    </template>
+    <template #footer> footer </template>
+  </PrimeCard>
+  <br />
 </template>
