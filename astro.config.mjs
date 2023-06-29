6d36367905a91247474631de97afa3c7f0d4bc5d
Jeudi Prando <jeudiprando@gmail.com>
2023-06-29 13:22:09 -0300

    vai

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    vue({
+      appEntrypoint: "/src/_vue",
+    }),
+    react(),
+    tailwind(),
+  ],
+});
