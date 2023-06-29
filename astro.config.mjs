e245fdc3311dad70ecad826fa0da85650ea18d91
Jeudi Prando <jeudiprando@gmail.com>
2023-06-29 14:38:24 -0300

    add ssr mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    vue({
-      appEntrypoint: "/src/_vue",
-    }),
-    react(),
-    tailwind(),
-  ],
+  integrations: [vue({
+    appEntrypoint: "/src/_vue"
+  }), react(), tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
